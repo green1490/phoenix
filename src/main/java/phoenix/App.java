84b755674597b16fe0258ee7f4867b1569ec8f02
green1490 <zambokrisztian1490@gmail.com>
2025-03-25 22:02:14 +0100

    libvirtd dependency

--- src/main/java/phoenix/App.java
+++ src/main/java/phoenix/App.java
@@ -4,10 +4,8 @@ import com.pulumi.Pulumi;
 import com.pulumi.core.Output;
 import com.pulumi.libvirt.Domain;
 import com.pulumi.libvirt.DomainArgs;
-import com.pulumi.libvirt.LibvirtFunctions;
 import com.pulumi.libvirt.Provider;
 import com.pulumi.libvirt.ProviderArgs;
-import com.pulumi.libvirt.inputs.DomainCpuArgs;
 import com.pulumi.libvirt.inputs.DomainDiskArgs;
 
 public class App {
@@ -25,16 +23,16 @@ public class App {
             var router = new Domain(
                 "Router", 
                 DomainArgs.builder()
-                .vcpu(3)
-                .memory(512)
+                .vcpu(3.0)
+                .memory(512.0)
                 .description("Routes traffic between 2 virtual machine")
                 .disks(
                     DomainDiskArgs.builder()
-                    .file("/var/lib/libvirt/images/kali-linux-2024.4-installer-amd64.iso")
+                    .url("https://dl-cdn.alpinelinux.org/alpine/v3.21/releases/cloud/nocloud_alpine-3.21.2-x86_64-bios-cloudinit-metal-r0.qcow2")
                     .build()
                     )
-                    .build()
-                    );
+                .build()
+                );
             ctx.export("Domain", Output.of("Domain"));
         });
     }
