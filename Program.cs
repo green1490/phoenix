86cf9772684800e074b3b2025a28ba930c9ceb0d
green1490 <zambokrisztian1490@gmail.com>
2025-05-20 23:00:58 +0200

    remove phoenix namespace

--- Program.cs
+++ Program.cs
@@ -11,33 +11,33 @@ return await Deployment.RunAsync(() =>
     .Configure<DeviceConfig>("Webserver", _ => 
         new DeviceConfig 
         {
-            Vcpu = config.RequireInt32("Webserver:Vcpu"),
-            Memory = config.RequireInt32("Webserver:Memory"),
-            Volume = config.Require("Webserver:Volume"),
-            Name = config.Require("Webserver:Name"),
-            Subnet = config.Require("Webserver:Subnet")
+            Vcpu = config.RequireInt32("Webserver.Vcpu"),
+            Memory = config.RequireInt32("Webserver.Memory"),
+            Volume = config.Require("Webserver.Volume"),
+            Name = config.Require("Webserver.Name"),
+            Subnet = config.Require("Webserver.Subnet")
         })
     .Configure<DeviceConfig>("Database", _ => 
         new DeviceConfig 
         {
-            Vcpu = config.RequireInt32("Database:Vcpu"),
-            Memory = config.RequireInt32("Database:Memory"),
-            Volume = config.Require("Database:Volume"),
-            Name = config.Require("Database:Name"),
-            Subnet = config.Require("Database:Subnet")
+            Vcpu = config.RequireInt32("Database.Vcpu"),
+            Memory = config.RequireInt32("Database.Memory"),
+            Volume = config.Require("Database.Volume"),
+            Name = config.Require("Database.Name"),
+            Subnet = config.Require("Database.Subnet")
         })
     .Configure<VolumeConfig>("Alpine", _ =>
         new VolumeConfig
         {
-            Src = config.Require("Alpine:Src"),
-            Name = config.Require("Alpine:Name")
+            Src = config.Require("Alpine.Src"),
+            Name = config.Require("Alpine.Name")
         })
     .AddSingleton<IDeviceFactory, DeviceFactory>()
     .AddSingleton<IVolumeFactory, VolumeFactory>()
     .BuildServiceProvider();
 
     var factory = services.GetRequiredService<IDeviceFactory>();
-    factory.CreateDevice("Webserver", "Alpine");
+    factory.CreateDevice("Webserver", "Alpine").Start();
 
     // Export outputs here
     return new Dictionary<string, object?>
